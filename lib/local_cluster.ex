3e5e0a792325b90c13c55c0192e152c472e10649
Santiago Tortosa <santitm99@gmail.com>
2022-11-08 17:33:22 +0100

    Replace :slave with :peer AND :rpc with :erpc

--- lib/local_cluster.ex
+++ lib/local_cluster.ex
@@ -12,24 +12,26 @@ defmodule LocalCluster do
   Starts the current node as a distributed node.
   """
   @spec start :: :ok
-  def start do
+  def start() do
     # boot server startup
     start_boot_server = fn ->
       # voodoo flag to generate a "started" atom flag
       :global_flags.once("local_cluster:started", fn ->
-        { :ok, _ } = :erl_boot_server.start([
-          { 127, 0, 0, 1 }
-        ])
+        {:ok, _} =
+          :erl_boot_server.start([
+            {127, 0, 0, 1}
+          ])
       end)
+
       :ok
     end
 
     # only ever handle the :erl_boot_server on the initial startup
-    case :net_kernel.start([ :"manager@127.0.0.1" ]) do
+    case :net_kernel.start([:"manager@127.0.0.1"]) do
       # handle nodes that have already been started elsewhere
-      { :error, { :already_started, _ } } -> start_boot_server.()
+      {:error, {:already_started, _}} -> start_boot_server.()
       # handle the node being started
-      { :ok, _ } -> start_boot_server.()
+      {:ok, _} -> start_boot_server.()
       # pass anything else
       anything -> anything
     end
@@ -54,52 +56,60 @@ defmodule LocalCluster do
   if you wish to spawn tasks from inside test code, as test code would
   not typically be loaded automatically.
   """
-  @spec start_nodes(binary, integer, Keyword.t) :: [ atom ]
+  @spec start_nodes(binary, integer, Keyword.t()) :: [atom]
   def start_nodes(prefix, amount, options \\ [])
-  when (is_binary(prefix) or is_atom(prefix)) and is_integer(amount) do
-    nodes = Enum.map(1..amount, fn idx ->
-      { :ok, name } = :slave.start_link(
-        '127.0.0.1',
-        :"#{prefix}#{idx}",
-        '-loader inet -hosts 127.0.0.1 -setcookie "#{:erlang.get_cookie()}"'
-      )
-      name
-    end)
+      when (is_binary(prefix) or is_atom(prefix)) and is_integer(amount) do
+    nodes =
+      Enum.map(1..amount, fn idx ->
+        options = %{
+          host: '127.0.0.1',
+          name: :"#{prefix}#{idx}",
+          args: ['-loader inet -hosts 127.0.0.1 -setcookie "#{:erlang.get_cookie()}"']
+        }
 
-    rpc = &({ _, [] } = :rpc.multicall(nodes, &1, &2, &3))
+        {:ok, peer, node} = :peer.start_link(options)
 
-    rpc.(:code, :add_paths, [ :code.get_path() ])
+        # Register the peer process locally on each node so that we can stop it later
+        persist_node_peer(node, peer)
 
-    rpc.(Application, :ensure_all_started, [ :mix ])
-    rpc.(Application, :ensure_all_started, [ :logger ])
+        node
+      end)
 
-    rpc.(Logger, :configure, [ [ level: Logger.level() ] ])
-    rpc.(Mix, :env, [ Mix.env() ])
+    rpc = &:erpc.multicall(nodes, &1, &2, &3)
 
-    loaded_apps = for { app_name, _, _ } <- Application.loaded_applications() do
-      base = Application.get_all_env(app_name)
+    rpc.(:code, :add_paths, [:code.get_path()])
 
-      environment =
-        options
-        |> Keyword.get(:environment, [])
-        |> Keyword.get(app_name, [])
-        |> Keyword.merge(base, fn _, v, _ -> v end)
+    rpc.(Application, :ensure_all_started, [:mix])
+    rpc.(Application, :ensure_all_started, [:logger])
 
-      for { key, val } <- environment do
-        rpc.(Application, :put_env, [ app_name, key, val ])
+    rpc.(Logger, :configure, [[level: Logger.level()]])
+    rpc.(Mix, :env, [Mix.env()])
+
+    loaded_apps =
+      for {app_name, _, _} <- Application.loaded_applications() do
+        base = Application.get_all_env(app_name)
+
+        environment =
+          options
+          |> Keyword.get(:environment, [])
+          |> Keyword.get(app_name, [])
+          |> Keyword.merge(base, fn _, v, _ -> v end)
+
+        for {key, val} <- environment do
+          rpc.(Application, :put_env, [app_name, key, val])
+        end
+
+        app_name
       end
-
-      app_name
-    end
 
     ordered_apps = Keyword.get(options, :applications, loaded_apps)
 
     for app_name <- ordered_apps, app_name in loaded_apps do
-      rpc.(Application, :ensure_all_started, [ app_name ])
+      rpc.(Application, :ensure_all_started, [app_name])
     end
 
     for file <- Keyword.get(options, :files, []) do
-      rpc.(Code, :require_file, [ file ])
+      rpc.(Code, :require_file, [file])
     end
 
     nodes
@@ -108,14 +118,37 @@ defmodule LocalCluster do
   @doc """
   Stops a set of child nodes.
   """
-  @spec stop_nodes([ atom ]) :: :ok
-  def stop_nodes(nodes) when is_list(nodes),
-    do: Enum.each(nodes, &:slave.stop/1)
+  @spec stop_nodes([atom]) :: :ok
+  def stop_nodes(nodes) when is_list(nodes) do
+    Enum.each(
+      nodes,
+      fn node ->
+        node
+        |> pop_node_peer()
+        |> :peer.stop()
+      end
+    )
+  end
 
   @doc """
   Stops the current distributed node and turns it back into a local node.
   """
-  @spec stop :: :ok | { :error, atom }
-  def stop,
-    do: :net_kernel.stop()
+  @spec stop :: :ok | {:error, atom}
+  def stop() do
+    :net_kernel.stop()
+  end
+
+  defp persist_node_peer(node, peer) do
+    :persistent_term.put({__MODULE__, node}, peer)
+  end
+
+  defp pop_node_peer(node) do
+    key = {__MODULE__, node}
+
+    peer = :persistent_term.get(key)
+
+    :persistent_term.erase(key)
+
+    peer
+  end
 end
